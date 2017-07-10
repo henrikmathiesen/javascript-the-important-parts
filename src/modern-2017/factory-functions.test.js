909b9fb6ab7f7f72868ac71a0da8c1a7fc7090f4
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-07-10 12:22:50 +0200

    -factory functions

--- src/modern-2017/factory-functions.test.js
+++ src/modern-2017/factory-functions.test.js
@@ -1,6 +1,14 @@
+const $ = require('jquery');
+
 describe('Factory functions creates objects and return them, can be used instead of classes, and are more simple', () => {
 
-    it('should first look at an ES6 class', () => {
+    beforeEach(() => {
+        const markup = '<div class="js-click-me">Click me</div>';
+        document.body.innerHTML = markup;
+    });
+
+    describe('We should first look at an ES6 class', () => {
+
         class Dog {
             constructor() {
                 this.sound = 'woof';
@@ -9,10 +17,94 @@ describe('Factory functions creates objects and return them, can be used instead
             talk() {
                 return this.sound;
             }
+
+            setSound() {
+                this.sound = 'woof2';
+            }
         }
 
-        const sniffles = new Dog();
-        expect(sniffles.talk()).toEqual('woof');
+        it('should work as expected', () => {
+            const sniffles = new Dog();
+            expect(sniffles.talk()).toEqual('woof');
+        });
+
+        it('will be a problem with the this pointer', () => {
+            const sniffles = new Dog();
+            const spy = jest.spyOn(sniffles, 'setSound');
+
+            $('.js-click-me').one('click', sniffles.setSound);
+            $('.js-click-me').trigger('click');
+
+            expect(spy).toHaveBeenCalled();
+            expect(sniffles.talk()).not.toEqual('woof2'); // new sound has NOT been set since this, inside setSound, now points to the DOM element
+        });
+
+        it('will be a problem with the this pointer that we can fix - A', () => {
+            const sniffles = new Dog();
+            const spy = jest.spyOn(sniffles, 'setSound');
+
+            $('.js-click-me').one('click', sniffles.setSound.bind(sniffles));
+            $('.js-click-me').trigger('click');
+
+            expect(spy).toHaveBeenCalled();
+            expect(sniffles.talk()).toEqual('woof2'); // new sound HAS been set since we force this to be sniffles
+        });
+
+        it('will be a problem with the this pointer that we can fix - B', () => {
+            const sniffles = new Dog();
+            const spy = jest.spyOn(sniffles, 'setSound');
+
+            $('.js-click-me').one('click', () => { sniffles.setSound() });
+            $('.js-click-me').trigger('click');
+
+            expect(spy).toHaveBeenCalled();
+            expect(sniffles.talk()).toEqual('woof2'); // new sound HAS been set since we wrapped setSound in a function
+        });
+
+        it('should also relies that this.sound is not a private member of the class', () => { 
+            const sniffles = new Dog();
+            expect(sniffles.talk()).toEqual('woof');
+
+            sniffles.sound = 'mjaa';
+            expect(sniffles.talk()).toEqual('mjaa');
+
+            // The only truly private data in JavaScript is still scoped variables
+            // https://stackoverflow.com/questions/22156326/private-properties-in-javascript-es6-classes
+        });
+
     });
 
+    describe('We should then look at factory functions', () => {
+
+        it('should work as expected', () => {
+            const dog = () => {
+                let _sound = 'woof';
+                let factory = {};
+
+                factory.talk = () => {
+                    return _sound;      // talk has access to sound because of closures
+                };
+
+                factory.setSound = () => {
+                    _sound = 'woof2';
+                };
+
+                return factory;
+            };
+
+            const sniffles = dog();
+            const spy = jest.spyOn(sniffles, 'setSound');
+
+            expect(sniffles.talk()).toEqual('woof');
+
+            $('.js-click-me').one('click', sniffles.setSound);
+            $('.js-click-me').trigger('click');
+
+            expect(spy).toHaveBeenCalled();
+            expect(sniffles.talk()).toEqual('woof2'); // it just works, because we do not use the this keyword
+        });
+
+        // also _sound is truly private to the dog function
+
+    });
 });
