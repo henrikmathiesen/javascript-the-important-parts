ab000d345e97b23dc8b0f7d642be5872da098a86
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-07-11 11:22:14 +0200

    -bind and this testing

--- src/modern-2017/object-creation/bind-and-this.test.js
+++ src/modern-2017/object-creation/bind-and-this.test.js
@@ -11,4 +11,33 @@ describe('bind and this', () => {
         expect(dog.talk()).toEqual('woff');
     });
 
+    it('should be aware of this pointer in this method also', () => {
+        const dog = {
+            sound: 'woff',
+            talk: function () { 
+                return this.sound;
+            }
+        };
+
+        // functions can be assigned to variables, but in this case we loose the this context
+        // javascripts function oriented nature clashes with its object oriented nature
+        // this has lost its connection to the dog object (it points to the global object)
+        const talkFunction = dog.talk;
+
+        expect(talkFunction()).toEqual(undefined);
+    });
+
+    it('should be aware of this pointer in this method can be fixed', () => {
+        const dog = {
+            sound: 'woff',
+            talk: function () { 
+                return this.sound;
+            }
+        };
+
+        const talkFunction = dog.talk.bind(dog);
+
+        expect(talkFunction()).toEqual('woff');
+    });
+
 });
