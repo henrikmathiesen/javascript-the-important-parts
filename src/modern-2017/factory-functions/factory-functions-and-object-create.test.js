3e3bf970dd9374041a9e4d9906b51d4b790f8745
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-07-16 21:15:32 +0200

    -todo comments

--- src/modern-2017/factory-functions/factory-functions-and-object-create.test.js
+++ src/modern-2017/factory-functions/factory-functions-and-object-create.test.js
@@ -1,10 +1,13 @@
 // todo
 // Read me, 3 bottom medium links
 
-describe('Factory functions and object.create', () => {
+describe('Factory functions and Object.create', () => {
 
-    it('', () => { 
-        
+    describe('First we should understand the difference between Object.create and Object.assign', () => { 
+        it('', () => {
+            // https://www.reddit.com/r/javascript/comments/425bc3/something_to_think_about_objectcreate_and/
+            // check instance of also ...
+        });
     });
 
 });
