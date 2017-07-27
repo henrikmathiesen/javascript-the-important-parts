2a8bde3c328451f5b65b18d4937db5bf321e9971
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-07-27 19:56:44 +0200

    -labbing on with webpack

--- src/modern-2017/webpack/webpack.config.js
+++ src/modern-2017/webpack/webpack.config.js
@@ -9,22 +9,25 @@
 */
 
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    //entry: path.resolve(__dirname, 'src/index.js'),   // 1 entry point
-    entry: {                                            // 2 entry points
+    //entry: path.resolve(__dirname, 'src/index.js'),                       // 1 entry point
+    entry: {                                                                // 2 entry points
         app: path.resolve(__dirname, 'src/index.js'),
         app2: path.resolve(__dirname, 'src/index-2.js'),
     },
     plugins: [
+        new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
-            title: 'Output Management'
+            template: path.resolve(__dirname, 'src/index.html')             // copy to dist and inject scripts/css (need to handle this together with templating)
         })
+        // new HtmlWebpackPlugin, for another html file
     ],
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js'                    // OBS [name]
+        filename: '[name].bundle.js'                                        // OBS [name] for generated entry point file
     },
     module: {
         rules: [
@@ -38,9 +41,9 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: [
-                    'style-loader',     // 3) injects css into DOM
-                    'css-loader',       // 2) enables import css file
-                    'sass-loader'       // 1) compiles sass to css
+                    'style-loader',                                         // 3) injects css into DOM
+                    'css-loader',                                           // 2) enables import css file
+                    'sass-loader'                                           // 1) compiles sass to css
                 ]
             },
             {
