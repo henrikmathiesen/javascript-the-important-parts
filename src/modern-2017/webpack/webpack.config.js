42d198dffff8743028a5090b45e2a6b55862b3b0
henrikmathiesen <henrik_mathiesen@hotmail.com>
2017-07-29 15:16:16 +0200

    -hashing css

--- src/modern-2017/webpack/webpack.config.js
+++ src/modern-2017/webpack/webpack.config.js
@@ -77,19 +77,19 @@ module.exports = (env) => {
     console.log('isProduction: ' + isProduction);
 
     return {
-        //entry: path.resolve(__dirname, 'src/index.js'),                                           // 1 entry point
-        entry: {                                                                                    // 2 entry points
+        //entry: path.resolve(__dirname, 'src/index.js'),                                                   // 1 entry point
+        entry: {                                                                                            // 2 entry points
             app: path.resolve(__dirname, 'src/index.js'),
             app2: path.resolve(__dirname, 'src/index-2.js'),
         },
-        devtool: !isProduction ? 'source-map' : false,                                              // cheap-module-eval-source-map is the faster option, but does not work with CSS
+        devtool: !isProduction ? 'source-map' : false,                                                      // cheap-module-eval-source-map is the faster option, but does not work with CSS
         plugins: [
             new CleanWebpackPlugin(['dist']),
-            new ExtractTextPlugin('[name].bundle.css'),                                             // extract css to seperate file, instead of injecting into DOM (see loader also)
+            new ExtractTextPlugin(isProduction ? '[name].[chunkhash].bundle.css' : '[name].bundle.css'),    // extract css to seperate file, instead of injecting into DOM (see loader also)
             new HtmlWebpackPlugin({
-                //hash: isProduction,                                                               // hash with query string (will be the same for all files, and hashes for all files update when 1 file change, not optimal)
-                //chunks: ['app'],                                                                  // only include app.js and app.css but not app2.js and app2.css
-                template: path.resolve(__dirname, 'src/index.html')                                 // copy to dist and inject scripts/css (need to handle this together with templating)
+                //hash: isProduction,                                                                       // hash with query string (will be the same for all files, and hashes for all files update when 1 file change, not optimal)
+                //chunks: ['app'],                                                                          // only include app.js and app.css but not app2.js and app2.css
+                template: path.resolve(__dirname, 'src/index.html')                                         // copy to dist and inject scripts/css (need to handle this together with templating)
             }),
             // new HtmlWebpackPlugin, for another html file (and can include different chunks into it)
 
@@ -114,17 +114,17 @@ module.exports = (env) => {
                     test: /\.js$/,
                     exclude: /node_modules/,
                     use: [
-                        'babel-loader',                                                             // 2) compile to ES5
-                        'eslint-loader'                                                             // 1) lint ES6
+                        'babel-loader',                                                                     // 2) compile to ES5
+                        'eslint-loader'                                                                     // 1) lint ES6
                     ]
                 },
                 {
                     test: /\.scss$/,
                     // use: [
-                    //     'style-loader',                                                          // 4) injects css into DOM
-                    //     'css-loader',                                                            // 3) enables import css file
-                    //     postCssLoader,                                                           // 2) post css transforms
-                    //     'sass-loader'                                                            // 1) compiles sass to css
+                    //     'style-loader',                                                                  // 4) injects css into DOM
+                    //     'css-loader',                                                                    // 3) enables import css file
+                    //     postCssLoader,                                                                   // 2) post css transforms
+                    //     'sass-loader'                                                                    // 1) compiles sass to css
                     // ]
 
                     // extract css to seperate file, instead of injecting into DOM (see plugins also)
