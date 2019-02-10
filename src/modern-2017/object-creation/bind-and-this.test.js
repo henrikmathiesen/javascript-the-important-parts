8f67b9e12d581493a692439f7c4bb4991b908eb5
henrikmathiesen <henrik_mathiesen@hotmail.com>
2019-02-10 10:50:30 +0100

    - this tests

--- src/modern-2017/object-creation/bind-and-this.test.js
+++ src/modern-2017/object-creation/bind-and-this.test.js
@@ -3,18 +3,33 @@ describe('bind and this', () => {
     it('should be aware of this pointer in this method', () => {
         const dog = {
             sound: 'woff',
-            talk: function () { 
+            talk: function () {
                 return this.sound;
+            },
+            getThis: function () { 
+                return this;
             }
         };
 
         expect(dog.talk()).toEqual('woff');
+        expect(dog.getThis()).toBe(dog);
+    });
+
+    it('should also be aware of this pointer in this method using arrow function', () => {
+        const dog = {
+            sound: 'woff',
+            talk: () => this.sound,
+            getThis: () => this
+        };
+
+        expect(dog.talk()).toEqual(undefined);
+        expect(dog.getThis()).toEqual({});          // No window object in Jest. Global object?
     });
 
     it('should be aware of this pointer in this method also', () => {
         const dog = {
             sound: 'woff',
-            talk: function () { 
+            talk: function () {
                 return this.sound;
             }
         };
@@ -30,7 +45,7 @@ describe('bind and this', () => {
     it('should be aware of this pointer in this method can be fixed', () => {
         const dog = {
             sound: 'woff',
-            talk: function () { 
+            talk: function () {
                 return this.sound;
             }
         };
