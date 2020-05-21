24ccca3073d89c85c5a80a61b5500d6af35d307a
henrikmathiesen <henrik_mathiesen@hotmail.com>
2020-05-21 14:28:44 +0200

    - document create element

--- src/other/create-insert-element/create-insert-element.js
+++ src/other/create-insert-element/create-insert-element.js
@@ -1,5 +1,18 @@
 (function () {
 
-    console.log('working');
+    //
+    // jQuery Way
+
+    $('#jq-target').append('<p>Iam text</p>');
+
+    //
+    // Vanilla Way
+    // https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+
+    const p = document.createElement('p');
+    const t = document.createTextNode('Iam text');
+    p.appendChild(t);
+
+    document.getElementById('js-target').appendChild(p);
 
 })();
